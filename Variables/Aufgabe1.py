e79b860fe715e28c15178fed3db017128b7694c0
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-02-21 15:08:31 -0300

    Update Aufgabe1.py
    Die vier ersten Aufgaben mit Variablen, deren Werten und deren Typen

--- Variables/Aufgabe1.py
+++ Variables/Aufgabe1.py
@@ -1,2 +1,21 @@
-# This is a comment
-print("Hello World!")
+# 1 Erstelle eine Variable mit dem Text: Hello World!
+# Gebe diese Variable aus
+
+
+# 2 Wechsle die Werte der beiden folgenden Variablen
+# Gebe die Werte aus
+x = 12
+y = 2000
+
+
+# 3 Was ist der Unterschied dieser beiden Variablen?
+x = 12
+y = "12"
+
+# 4 Teste die vier grundlegenden Mathe-Operationen
+# Das sind:
+# Division:         /
+# Multiplikation:   *
+# Addition:         +
+# Subtraktion:      -
+# Fällt dir beim ausgeben der Ergebnisse etwas auf?
