4c99752791307bd32fcb8a005d62e280a784c7fd
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-02-27 08:57:05 -0300

    Create Aufgabe2.py
    Erklärung von floats hinzugefügt

---
+++ Variables/Aufgabe2.py
@@ -0,0 +1,7 @@
+# Komma-Zahlen erhalten einen speziellen Typ: float
+# Initialisiere eine Float-Variable mit folgendem Befehl:
+number = float(12)
+# Nachkommastellen bei dieser Initialisierung mit einem ".":
+number2 = float(12.2)
+# Zahlen können auch innerhalb einer "Operation" stattfinden:
+number3 = 12 / float(5)
