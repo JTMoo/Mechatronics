b1d7727e87711f1a73d5bd4176ec5bc46507d7b8
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-02-27 09:22:07 -0300

    Update Aufgabe2.py
    Aufgaben mit Kommazahlen hinzugefügt

--- Variables/Aufgabe2.py
+++ Variables/Aufgabe2.py
@@ -1,7 +1,18 @@
-# Komma-Zahlen erhalten einen speziellen Typ: float
-# Initialisiere eine Float-Variable mit folgendem Befehl:
-number = float(12)
-# Nachkommastellen bei dieser Initialisierung mit einem ".":
-number2 = float(12.2)
-# Zahlen können auch innerhalb einer "Operation" stattfinden:
-number3 = 12 / float(5)
+# |-----------------------------------------------------------------|
+# |Komma-Zahlen erhalten einen speziellen Typ: float                |
+# |Initialisiere eine Float-Variable mit folgendem Befehl:          |
+# | float(Zahl)                                                     |
+# |Nachkommastellen bei dieser Initialisierung mit einem ".":       |
+# | float(12.2)                                                     |
+# |Zahlen können auch innerhalb einer "Operation" stattfinden:      |
+# |Zahl / float(Zahl2)                                              |
+# |-----------------------------------------------------------------|
+
+# 1 Gebe das genaue Ergebnis der Rechnung 12/5 aus.
+
+# 2 Finde eine Operation (mithilfe einer Suchmaschine), die Zahlen runden kann.
+# Gebe das Ergebnis von 1/3 auf 2 Nachkommastellen genau aus.
+
+# 3.1 Wenn ein Arbeiter am Tag 25 Steine von A nach B bringen kann, wie viele Steine
+# bewegt er dann in der Stunde? (Achtung: hier muss eine Annahme getroffen werden)
+# 3.2 Wie weit ist A von B entfernt? (Mehrere Annahmen nötig, Annahmen müssen begründet werden)
