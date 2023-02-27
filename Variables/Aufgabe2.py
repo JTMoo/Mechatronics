bada1ecb18313aebede10e8249b2e5895aba690a
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-02-27 09:29:31 -0300

    Update Aufgabe2.py
    Update zu Aufgabe 3

--- Variables/Aufgabe2.py
+++ Variables/Aufgabe2.py
@@ -16,3 +16,11 @@
 # 3.1 Wenn ein Arbeiter am Tag 25 Steine von A nach B bringen kann, wie viele Steine
 # bewegt er dann in der Stunde? (Achtung: hier muss eine Annahme getroffen werden)
 # 3.2 Wie weit ist A von B entfernt? (Mehrere Annahmen nötig, Annahmen müssen begründet werden)
+# 3.3 Ist ein anderer Arbeiter, der 15 Steine am Tag, über eine Entfernung von 1km trägt schneller oder langsamer?
+# |---------------------------------------------------------------------------------------------|
+# | Info zu Aufgabe 3:                                                                          |
+# | - Alle Ergebnisse mit einem print ausgeben                                                  |
+# | - Es muss eine ausführbare Datei erstellt werden, die immer die gleichen Ergebnisse liefert |
+# | - Für Aufgabe 3.3 müssen Vergleichsoperatoren ('<', '>' oder '==') verwendet werden         |
+# |   Diese sind in der gleichnamigen Datei mit dieser Datei dokumentiert                       |
+# |---------------------------------------------------------------------------------------------|
