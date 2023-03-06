afa8aefb7df0de33ba2d90c5a596abaf8deb4429
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-06 15:18:56 -0300

    Create Aufgabenblatt4.py

---
+++ Conditions/Aufgabenblatt4.py
@@ -0,0 +1,2 @@
+# 1 Schreibe ein Programm, dass eine (beliebige) Frage ausgibt und dann eine Antwort des Benutzers abwartet.
+#   Pruefe anschliessend, ob die Frage richtig oder falsch beantwortet wurde und setze den Benutzer entsprechend in Kenntnis.
