4336ebb1db138436de751c63f8297bdeaedce219
GioVeiluva <126104533+GioVeiluva@users.noreply.github.com>
2023-03-08 15:51:54 -0300

    Create Aufgabenblatt1.py

---
+++ _GVeiluva/Aufgabenblatt1.py
@@ -0,0 +1,37 @@
+# 1 Erstelle eine Variable mit dem Text: Hello World!
+# Gebe diese Variable aus
+MeineVariable="HelloWorld"
+print(MeineVariable)
+
+# 2 Wechsle die Werte der beiden folgenden Variablen
+# Gebe die Werte aus
+x = 12
+y = 2000
+
+x = 2000
+print(x)
+y = 12
+print(y)
+
+# 3 Was ist der Unterschied dieser beiden Variablen?
+x = 12
+print(x)
+y = "12"
+print(y)
+# X ist eine Zahl und Y ist ein Satz.
+
+# 4 Teste die vier grundlegenden Mathe-Operationen
+# Das sind:
+# Division:         /
+# Multiplikation:   *
+# Addition:         +
+# Subtraktion:      -
+# Fällt dir beim ausgeben der Ergebnisse etwas auf?
+x=14
+y=7
+print(x/y)
+print(x*y)
+print(x+y)
+print(x-y)
+
+# Mir fällt nichts wirklich bei den Ergebnissen auf
