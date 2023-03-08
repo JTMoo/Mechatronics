d0ce6669bede171355c9b9630b85fb5fb478b5c8
Nastasiakoop <126104798+Nastasiakoop@users.noreply.github.com>
2023-03-08 15:34:57 -0300

    Update Aufgabenblatt1.py

--- _NKoop/Aufgabenblatt1.py
+++ _NKoop/Aufgabenblatt1.py
@@ -1,1 +1,34 @@
+# 1 Erstelle eine Variable mit dem Text: Hello World!
+# Gebe diese Variable aus
+HelloWorldVariable = "Hello World"
+print (HelloWorldVariable)
 
+# 2 Wechsle die Werte der beiden folgenden Variablen
+# Gebe die Werte aus
+x = 12
+y = 2000
+
+x= 2000
+y= 12
+print (x)
+print (y)
+
+# 3 Was ist der Unterschied dieser beiden Variablen?
+x = 12
+y = "12"
+# x ist ein int oder eine Zahl und y ist ein shring oder ein Buchstabe.
+
+# 4 Teste die vier grundlegenden Mathe-Operationen
+# Das sind:
+# Division:         /
+# Multiplikation:   *
+# Addition:         +
+# Subtraktion:      -
+# Fällt dir beim ausgeben der Ergebnisse etwas auf?
+x=12
+y=3
+print (x/y)
+print (x*y)
+print (x+y)
+print (x-y)
+# Alles funktioniert.
