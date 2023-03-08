3bb30ecfc4ab6c22d29807fae5c9813b1524904b
Manu_J <109835226+ManuChicken@users.noreply.github.com>
2023-03-08 16:00:10 -0300

    Update Aufgabenblatt1.py

--- _MJanz/Aufgabenblatt1.py
+++ _MJanz/Aufgabenblatt1.py
@@ -1,1 +1,30 @@
+# 1 Erstelle eine Variable mit dem Text: Hello World!
+# Gebe diese Variable aus
+# MeineErsteVar = "Hello World!"
 
+# 2 Wechsle die Werte der beiden folgenden Variablen
+# Gebe die Werte aus
+# x = 2000
+# y = 12
+
+# 3 Was ist der Unterschied dieser beiden Variablen?
+# x = 12
+# y = "12"
+# Die Variable y ist ein Text
+
+# 4 Teste die vier grundlegenden Mathe-Operationen
+# Das sind:
+# Division:         /
+# Multiplikation:   *
+# Addition:         +
+# Subtraktion:      -
+
+y = 24
+x = 12
+print(y/x)
+print(y*x)
+print(y+x)
+print(y-x)
+
+# Fällt dir beim ausgeben der Ergebnisse etwas auf?
+# Dass beim Teilen die Zahl immer eine Kommazahl ist 
