f0b5ea47361146c32cae9423dbde5d93b1ed694f
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-09 15:00:34 -0300

    Create Test1.py

---
+++ Python/Advanced/Test1.py
@@ -0,0 +1,9 @@
+# Erstelle eine Klasse 'User', die folgende Eigenschaften hat:
+# Age, Name, Surname, Job und Joined
+# Joined ist dabei der Zeitstempel, wann der User angelegt wurde.
+# Die Klasse soll folgende Methoden haben:
+# 1. Konstruktor mit allen Age, Name, Surname and Job
+# 2. Konstruktor mit Alter, vollem Namen und Beruf
+# 3. get_full_name für die Ausgabe des vollen Namens
+
+# Erstelle eine Liste von drei Beispiel-Usern
