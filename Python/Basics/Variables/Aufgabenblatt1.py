3ef42a54ff63d55e960e2ee15e1cfed988577ad2
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-09 14:11:30 -0300

    Create Aufgabenblatt1.py

---
+++ Python/Basics/Variables/Aufgabenblatt1.py
@@ -0,0 +1,21 @@
+# 1 Erstelle eine Variable mit dem Text: Hello World!
+# Gebe diese Variable aus (Das ausgeben von Variablen ist in der Datei 'Introduction' beschrieben
+
+
+# 3 Was ist der Unterschied dieser beiden Variablen?
+x = 12
+y = "12"
+
+# 4 Teste die vier grundlegenden Mathe-Operationen
+# Das sind:
+# Division:         /
+# Multiplikation:   *
+# Addition:         +
+# Subtraktion:      -
+# Gebe die Ergebnisse folgender Rechnungen aus:
+x = 3
+y = 4
+div = x / y
+mul = x * y
+add = 12 + 8
+sub = 5 - 8
