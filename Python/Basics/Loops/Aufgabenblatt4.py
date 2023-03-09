7eb8fe12255db1fffde9adedbffc4b95c162fcb8
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-09 14:45:42 -0300

    Create Aufgabenblatt4.py

---
+++ Python/Basics/Loops/Aufgabenblatt4.py
@@ -0,0 +1,4 @@
+# 1 Schreibe ein Programm, dass alle Zahlen zwischen 1 und der einer vom Benutzer definierten Zahl ausgibt.
+#   Achtung: Fehlerfaelle beruecksichtigen
+
+# 2 Schreibe ein Programm das alle Primzahlen zwischen 1 und 1000 ausgibt.
