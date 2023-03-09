fe34e8684ad66a963faa1346a20ac528e26841b0
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-09 14:35:46 -0300

    Create _bool.py

---
+++ Python/Basics/Variables/_bool.py
@@ -0,0 +1,10 @@
+# |-----------------------------------------------------------------|
+# |Für Schwellwerte bzw. binäre Darstellungen verwendet man 'bool'  |
+# |-----------------------------------------------------------------|
+x = 12 < 5      # Initialisierung der Variablen 'x'
+print(type(x))  # <class 'bool'>
+x = True        # Zuweisung der Variablen 'x' mit dem Wert True
+print(type(x))  # <class 'bool'>
+
+# 1 Das Ergebnis der Rechnung 100/0.00002 ist größer als 1.000.000.
+# Ist das wahr? Gebe das Ergebnis des Vergleichs als 'bool aus.
