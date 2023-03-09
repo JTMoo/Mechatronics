798789a38cca604b49c04d7c58efcabcee27e3a4
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-09 14:27:51 -0300

    Create _floats.py

---
+++ Python/Basics/Variables/_floats.py
@@ -0,0 +1,10 @@
+# |-----------------------------------------------------------------|
+# |Komma-Zahlen erhalten einen speziellen Typ: float                |
+# |-----------------------------------------------------------------|
+x = 12.2        # x ist hier vom Typ float.
+print(type(x))  # <class 'float'>
+x = 12          # Hier wird die Variable 'x' neu intialisiert als Typ int!
+print(type(x))  # <class 'int'>
+
+# 1 Finde eine Operation (mithilfe einer Suchmaschine aka. Google o.Ä.), die Zahlen runden kann.
+# Gebe das Ergebnis von 1/3 auf 2 Nachkommastellen genau aus.
