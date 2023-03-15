97eaaf16c9a34480babbc36f3d3fa65c5cda0a81
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-15 08:12:17 -0300

    Update Aufgabenblatt4.py

--- Python/Basics/Loops/Aufgabenblatt4.py
+++ Python/Basics/Loops/Aufgabenblatt4.py
@@ -1,4 +1,7 @@
-# 1 Schreibe ein Programm, dass alle Zahlen zwischen 1 und der einer vom Benutzer definierten Zahl ausgibt.
+# 1 Schreibe ein Programm, das alle Zahlen zwischen 1 und der einer vom Benutzer definierten Zahl ausgibt.
 #   Achtung: Fehlerfaelle beruecksichtigen
 
-# 2 Schreibe ein Programm das alle Primzahlen zwischen 1 und 1000 ausgibt.
+# 2 Schreibe ein Programm, das alle Primzahlen zwischen 1 und 1000 ausgibt.
+
+# 3 Schreibe ein Programm, das alle Zeichen eines Benutzer-definierten strings in jeweils einer Zeile ausgibt.
+#   Hinweis: Benutze '\n'
