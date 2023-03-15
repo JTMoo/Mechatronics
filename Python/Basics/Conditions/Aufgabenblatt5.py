10d80da8f5c545a64ec4687676523d0f49130dac
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-03-15 08:31:16 -0300

    Update Aufgabenblatt5.py

--- Python/Basics/Conditions/Aufgabenblatt5.py
+++ Python/Basics/Conditions/Aufgabenblatt5.py
@@ -1,2 +1,5 @@
-# 1 Schreibe ein Programm, dass eine (beliebige) Frage ausgibt und dann eine Antwort des Benutzers abwartet.
+# 1 Wie kann das Programm aus Aufgabenblatt3 Aufgabe 3 verbessert werden, sodass kein Fehler mehr auftreten kann?
+#   Schreibe dieses Programm.
+
+# 2 Schreibe ein Programm, dass eine (beliebige) Frage ausgibt und dann eine Antwort des Benutzers abwartet.
 #   Pruefe anschliessend, ob die Frage richtig oder falsch beantwortet wurde und setze den Benutzer entsprechend in Kenntnis.
