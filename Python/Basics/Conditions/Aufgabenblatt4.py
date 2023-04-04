096d5cdc37bc544d843b9b6337d0d5136f2d005c
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-04-04 06:55:47 -0400

    Update and rename Aufgabenblatt5.py to Aufgabenblatt4.py

--- Python/Basics/Conditions/Aufgabenblatt4.py
+++ Python/Basics/Conditions/Aufgabenblatt4.py
@@ -3,3 +3,6 @@
 
 # 2 Schreibe ein Programm, dass eine (beliebige) Frage ausgibt und dann eine Antwort des Benutzers abwartet.
 #   Pruefe anschliessend, ob die Frage richtig oder falsch beantwortet wurde und setze den Benutzer entsprechend in Kenntnis.
+
+# 3 Wie kann die Ausgabe aus Aufgabenblatt2 Aufgabe 3 verbessert werden?
+#   Wenn die Anzahl der Steine von Arbeiter 1 verändert werden und sich der "schnellere Arbeiter" ändert, jeweils eine separate Ausgabe erscheinen.
