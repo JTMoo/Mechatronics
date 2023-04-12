64b626e8311187a3fbe6bf0a658b0ba005045e28
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-04-12 15:31:24 -0400

    Update Aufgabenblatt3.py

--- Python/Basics/UserInput/Aufgabenblatt3.py
+++ Python/Basics/UserInput/Aufgabenblatt3.py
@@ -4,8 +4,8 @@
 # | Verwende einen Zeilenumbruch im string mit '\n'             |
 # |-------------------------------------------------------------|
 
-# 2 Modifiziere die dritte Aufgabe in Aufgabenblatt2 so, dass die Anzahl der Steine als Benutzereingabe definiert ist.
-#   Bestimme die Anzahl der Steine, bei dem der schnellere Arbeiter wechselt.
+# 2 Passe die 1.Aufgabe in Aufgabenblatt2 so an, dass die Anzahl der Steine als Benutzereingabe definiert ist.
+#   Bestimme die Anzahl der Steine, bei dem der schnellere Arbeiter wechselt. (Beispiel: Wenn Arbeiter 1 15 Steine am Tag traegt, dann ist er schneller als Arbeiter 2)
 
 # 3 Verwende eine Benutzereingabe, um eine Division durchzuführen.
-# 3.1 Muss die Benutzereingabe kontrolliert werden? Wenn ja, warum?
+# 3.1 Muss die Benutzereingabe kontrolliert werden? Wenn ja, warum? (Hier muss nicht programmiert werden)
