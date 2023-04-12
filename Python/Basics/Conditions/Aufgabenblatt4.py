521efc0a3af33cf76751cd049a610edf3fb25ab8
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-04-12 15:43:31 -0400

    Update Aufgabenblatt4.py

--- Python/Basics/Conditions/Aufgabenblatt4.py
+++ Python/Basics/Conditions/Aufgabenblatt4.py
@@ -1,8 +1,9 @@
-# 1 Wie kann das Programm aus Aufgabenblatt3 Aufgabe 3 verbessert werden, sodass kein Fehler mehr auftreten kann?
+# 1 Wenn bei der Division (Teilen) der 3. Aufgabe, die eingelesene Zahl im Nenner steht (eingelesene_Zahl = input().... div = Zahl2 / eingelesene_Zahl), in welchem Fall schlaegt die Division dann fehl?
+#   Wie kann durch eine Bedingung, dieser Fall verhindert werden?
 #   Schreibe dieses Programm.
 
 # 2 Schreibe ein Programm, dass eine (beliebige) Frage ausgibt und dann eine Antwort des Benutzers abwartet.
-#   Pruefe anschliessend, ob die Frage richtig oder falsch beantwortet wurde und setze den Benutzer entsprechend in Kenntnis.
+#   Pruefe mihilfe einer Bedingung danach, ob die Frage richtig oder falsch beantwortet wurde und informiere den Benutzer mit der print-Funktion darueber.
 
-# 3 Wie kann die Ausgabe aus Aufgabenblatt2 Aufgabe 3 verbessert werden?
-#   Wenn die Anzahl der Steine von Arbeiter 1 verändert werden und sich der "schnellere Arbeiter" ändert, jeweils eine separate Ausgabe erscheinen.
+# 3 Verbessere die Ausgabe des schnelleren Arbeiters aus der 3. Aufgabe in Aufgabenblatt2.
+#   Verwende eine Bedingung, um unterschiedliche Ausgaben (print) im "Wenn" (if) und "Sonst" (else) Fall auszugeben.
