152e97ee53e771fbd688bdb32fe7ed91b08de280
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-04-18 14:45:53 -0400

    Create Übung_Listen.py

---
+++ Python/Basics/Container/Übung_Listen.py
@@ -0,0 +1,5 @@
+# 1 Schreibe eine Liste mit 3 Zahlen (int) als Elemente dieser Liste.
+
+# 2 Erfasse 2 weitere Zahlen über Benutzereingaben (input) und hänge diese hinten an die vorhandene Liste an.
+
+# 3 Gebe die angefügte Zahlen und die Länge der Liste aus. (print)
