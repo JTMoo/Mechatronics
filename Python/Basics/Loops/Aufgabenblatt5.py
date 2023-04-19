de1f2112d4946ad96b8e1e6d02597d6e87e45dcb
JTMoo <65012136+JTMoo@users.noreply.github.com>
2023-04-19 14:35:19 -0400

    Update Aufgabenblatt5.py

--- Python/Basics/Loops/Aufgabenblatt5.py
+++ Python/Basics/Loops/Aufgabenblatt5.py
@@ -1,7 +1,8 @@
-# 1 Schreibe ein Programm, das alle Zahlen zwischen 1 und der einer vom Benutzer definierten Zahl ausgibt.
-#   Achtung: Fehlerfaelle beruecksichtigen
+# 1 Schreibe ein Programm, das alle Zahlen zwischen 1 und der einer vom Benutzer definierten Zahl (input) ausgibt.
+#   Achtung: Fehlerfaelle beruecksichtigen (Bedingungen)
 
 # 2 Schreibe ein Programm, das alle Primzahlen zwischen 1 und 1000 ausgibt.
+#   Primzahlen sind so definiert, dass sie nur durch sich selbst und 1 teilbar sind. Beispiel: 1, 2, 3, 5, 7 ....
 
-# 3 Schreibe ein Programm, das alle Zeichen eines Benutzer-definierten strings in jeweils einer Zeile ausgibt.
-#   Hinweis: Benutze '\n'
+# 3 Schreibe ein Programm, das alle Zeichen eines Benutzer-definierten (input) strings (Bedingung) in jeweils einer Zeile ausgibt.
+#   Hinweis: Benutze '\n' fuer einen Zeilenumbruch
