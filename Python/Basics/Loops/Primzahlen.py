25b50d398c2b50cb737a14df5a4ef17e2e2174e7
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-24 15:43:46 -0400

    Update Primzahlen.py

--- Python/Basics/Loops/Primzahlen.py
+++ Python/Basics/Loops/Primzahlen.py
@@ -4,7 +4,7 @@ if numberToCheck > 1:
 	for i in range(2, int(numberToCheck / 2) + 1):
 		if (numberToCheck % i) == 0:
 			print(numberToCheck, "ist keine Primzahl")
-		break
+			break
 	else:
 		print(numberToCheck, "ist eine Primzahl")
 else:
