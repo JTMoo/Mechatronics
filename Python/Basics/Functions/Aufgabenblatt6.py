7ba497bc69807821e86f71aee651c07470a9af63
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-24 14:36:21 -0400

    Update Aufgabenblatt6.py

--- Python/Basics/Functions/Aufgabenblatt6.py
+++ Python/Basics/Functions/Aufgabenblatt6.py
@@ -3,3 +3,7 @@
 
 # 2 Schreibe eine Funktion, die eine Zahl als Uebergabeparameter hat. Ueberpruefe in der Funktion, ob die Zahl eine Primzahl ist.
 #   Kopiere das Programm von Aufgabenblatt5-Aufgabe2 und schreibe es mit deiner neuen Funktion um.
+
+# 3 Schreibe eine Funktion in einem neuen Programm (neue Datei), die den Rueckgabeparameter der input-Funktion immer in einen string umwandelt.
+#   Nenne diese Funktion "input_string".
+#   Teste diese Funktion in dem Programm, in dem du Aufgabe 1 und 2 geloest hast. (D.h. die Funktion einmal ausfuehren)
