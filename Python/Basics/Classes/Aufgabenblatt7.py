37eb2e7a3b79b277b6e6ee766a66027b19927ca0
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-31 10:14:23 -0400

    Create Aufgabenblatt7.py

---
+++ Python/Basics/Classes/Aufgabenblatt7.py
@@ -0,0 +1,4 @@
+# 1 Erstelle eine Klasse "Benutzer", die drei Variablen "Name, Alter und Lieblingsfarbe" enthält.
+#   Dabei muss Name und Lieblingsfarbe ein string sein und das Alter ein int.
+#   Erstelle eine Liste mit 5 dieser Benutzer und gebe jeweils den Namen und die Lieblingsfarbe in einem Satz aus.
+#   Beispiel für einen Satz: "Jakob mag die Farbe Orange"
