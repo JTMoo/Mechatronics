67ae4d0a61948daa9fda1c8578805dc2c933d667
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-31 14:50:39 -0400

    Update Aufgabenblatt6.py

--- Python/Basics/Functions/Aufgabenblatt6.py
+++ Python/Basics/Functions/Aufgabenblatt6.py
@@ -1,4 +1,4 @@
-# 1 Schreibe eine Funktion (Mit dem Namen add), die zwei Zahlen als Uebergabeparameter hat. Addiere diese beiden Zahlen und gebe das Ergebnis zurueck.
+# 1 Schreibe eine Funktion (Mit dem Namen add), die zwei Zahlen als Uebergabeparameter hat. Die Funktion soll diese beiden Zahlen addieren und dann das Ergebnis zurueckgeben.
 #   Verwende diese Funktion in einem Programm, dass zwei Zahlen einliesst (input), diese Zahlen der Funktion (add) uebergibt und anschliessend das Ergebnis ausgibt (print).
 
 # 2 Schreibe eine Funktion, die eine Zahl als Uebergabeparameter hat. Ueberpruefe in der Funktion, ob die Zahl eine Primzahl ist.
