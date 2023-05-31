fa2d64383d70adb8133ed3fe8b552cb8e154c0d9
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-31 15:14:58 -0400

    Update Rätsel4.py

--- Rätsel/Rätsel4.py
+++ Rätsel/Rätsel4.py
@@ -7,3 +7,10 @@ def function1(var1):
   
 var2 = 0
 function1(var2)
+
+# Diese Funktion zahlt von 99-1 rueckwarts zu null.
+# Das Prinzip, dass sich hinter dieser Funktion verbirgt nennt man "Rekursion".
+# Bei einer Rekursion ruft eine Funktion sich immer wieder selbst auf. (In unserem Fall ist der Aufruf von function1 in der Definition von function1 enthalten)
+# Damit die Funktion nicht in einer Endlosschleife endet (Immer wiederkehrender Aufruf ohne Ende), ist ein Anker eingebaut, bei dem die Funktion abbricht.
+# Auf den ersten Blick muesste das Programm eigentlich von 0 hoch zaehlen (Ausgabe von var1 im Programm). 
+# Veranschauliche in einer Zeichnung (Von Hand, Paint oder Aehnliches), warum die Funktion von 99-1 abwaerts zeahlt.
