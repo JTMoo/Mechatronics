43918d301ac708637a643e02291bf781bbaa3afa
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-05-31 15:00:43 -0400

    Update Aufgabenblatt6.py|

--- Python/Basics/Functions/Aufgabenblatt6.py
+++ Python/Basics/Functions/Aufgabenblatt6.py
@@ -1,9 +1,10 @@
 # 1 Schreibe eine Funktion (Mit dem Namen add), die zwei Zahlen als Uebergabeparameter hat. Die Funktion soll diese beiden Zahlen addieren und dann das Ergebnis zurueckgeben.
 #   Verwende diese Funktion in einem Programm, dass zwei Zahlen einliesst (input), diese Zahlen der Funktion (add) uebergibt und anschliessend das Ergebnis ausgibt (print).
 
-# 2 Schreibe eine Funktion, die eine Zahl als Uebergabeparameter hat. Ueberpruefe in der Funktion, ob die Zahl eine Primzahl ist.
+# 2 Schreibe eine Funktion, die eine Zahl als Uebergabeparameter hat. Ueberpruefe in der Funktion, ob die Zahl eine Primzahl ist. (Inhalt der Funktion ist in Primzahlen.py-Datei)
 #   Kopiere das Programm von Aufgabenblatt5-Aufgabe2 und schreibe es mit deiner neuen Funktion um.
+#   Rueckgabeparameter der Funktion soll ein bool sein. (True oder False)
 
-# 3 Schreibe eine Funktion in einem neuen Programm (neue Datei), die den Rueckgabeparameter der input-Funktion immer in einen string umwandelt.
-#   Nenne diese Funktion "input_string".
+# 3 Schreibe eine Funktion in einem neuen Programm (neue Datei), die den Rueckgabeparameter der input-Funktion immer in einen int umwandelt.
+#   Nenne diese Funktion "input_int". (import Dateiname)
 #   Teste diese Funktion in dem Programm, in dem du Aufgabe 1 und 2 geloest hast. (D.h. die Funktion einmal ausfuehren)
