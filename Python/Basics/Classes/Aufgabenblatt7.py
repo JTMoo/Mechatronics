6e7f03d35ae3a739718bdd01b2f45f30a8a9edb4
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-06-07 14:44:41 -0400

    Update Aufgabenblatt7.py

--- Python/Basics/Classes/Aufgabenblatt7.py
+++ Python/Basics/Classes/Aufgabenblatt7.py
@@ -2,3 +2,7 @@
 #   Dabei muss Name und Lieblingsfarbe ein string sein und das Alter ein int.
 #   Erstelle eine Liste mit 5 dieser Benutzer und gebe jeweils den Namen und die Lieblingsfarbe in einem Satz aus.
 #   Beispiel für einen Satz: "Jakob mag die Farbe Orange"
+
+# 2 Erstelle eine Klasse Hund, die eine Variable "Name" enthaelt.
+#   Schreibe eine Funktion innerhalb dieser Klasse mit dem Namen "sit".
+#   Diese Funktion soll den folgenden Satz ausgeben: "Name des Hundes" sitzt.
