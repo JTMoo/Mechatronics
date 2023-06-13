c5d62a5283c841d4434801942c0bfd1f83f8ae2a
Jonathan <65012136+JTMoo@users.noreply.github.com>
2023-06-13 09:36:14 -0400

    Update Rätsel2.py

--- Rätsel/Rätsel2.py
+++ Rätsel/Rätsel2.py
@@ -1,6 +1,4 @@
-# Zwischen zwei Stämmen ist ein Seil gespannt.
-# Die Länge des Seils ist 10m.
-# Das Seil ist jeweils in 5m Höhe angebracht.
+# Zwischen zwei Stämmen ist ein Seil gespannt, das 10 Meter lang ist.
 # Wie weit sind die beiden Stämme voneinander entfernt, wenn das Seil in der Mitte genau 5m durchhängt?
 # Darstellung:
 #     |         |
